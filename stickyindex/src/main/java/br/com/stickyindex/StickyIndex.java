c2db6520c6ab866655981e81894997adf47c4b7c
medvedomg <zhdanov.vadim.alexandrovich@gmail.com>
2017-12-23 20:38:08 +0200

    update textsize

--- stickyindex/src/main/java/br/com/stickyindex/StickyIndex.java
+++ stickyindex/src/main/java/br/com/stickyindex/StickyIndex.java
@@ -118,13 +118,13 @@ public class StickyIndex extends RelativeLayout {
     }
 
     private void setStickyIndexStyle (IndexAdapter.RowStyle styles) {
-        if (styles.getRowHeigh() != -1) {
+//        if (styles.getRowHeigh() != -1) {
             LinearLayout stickyIndexWrapper = (LinearLayout) this.findViewById(R.id.sticky_index_wrapper);
             android.view.ViewGroup.LayoutParams params = stickyIndexWrapper.getLayoutParams();
             params.height = 40;
             params.width = 40;
             stickyIndexWrapper.setLayoutParams(params);
-        }
+//        }
 
         if (styles.getTextSize() != -1) {
             stickyIndex.getStickyIndex().setTextSize(TypedValue.COMPLEX_UNIT_PX, styles.getTextSize());
